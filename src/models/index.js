338f14ae5b30aa5b22ec323de78aee1298aa77b2
VladMorzhanov <v.morzhanov@mevics.com>
2017-10-24 16:55:46 +0300

    added http server support if we don't need ssl layer

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,3 @@
 module.exports = {
-  Settings: require('./Settings'),
-  Stats: require('./Stats'),
-  User: require('./User'),
-  Author: require('./daily/Author'),
-  BlogPost: require('./daily/BlogPost'),
-  DailyPost: require('./daily/DailyPost'),
-  EmailResetToken: require('./token/EmailResetToken'),
-  EmailVerifyToken: require('./token/EmailVerifyToken'),
-  PasswordResetToken: require('./token/PasswordResetToken'),
-  Expert: require('./daily/Expert')
+  User: require('./User')
 }
